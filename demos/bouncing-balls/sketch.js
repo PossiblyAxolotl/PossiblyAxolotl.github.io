5ced55c7c5ed950aa7a1bbd2c477671440efe2fc
PossiblyAxolotl <me@possiblyaxolotl.com>
2025-03-06 13:59:50 -0600

    made the code longer

--- demos/bouncing-balls/sketch.js
+++ demos/bouncing-balls/sketch.js
@@ -12,21 +12,7 @@ function setup() {
 function draw() {
   background(220);
 
-  for (let ball of ballArray) {
-    // move ball
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-
-    // teleport around screen
-    if (ball.x > width + ball.radius) {ball.x -= width + ball.radius*2}
-    else if (ball.x < -ball.radius) {ball.x += width + ball.radius*2}
-
-    if (ball.y > height + ball.radius) {ball.y -= height + ball.radius*2}
-    else if (ball.y < -ball.radius) {ball.y += height + ball.radius*2}
-
-    // draw ball
-    circle(ball.x, ball.y, ball.radius*2);
-  }
+  processBalls();
 }
 
 function mousePressed() {
@@ -44,4 +30,36 @@ function spawnBall() {
   };
 
   ballArray.push(newBall);
+}
+
+function processBalls() {
+  for (let ball of ballArray) {
+    moveBall(ball);
+    drawBall(ball);
+  }
+}
+
+function moveBall(ball) {
+  // move ball
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  // teleport around screen
+  if (ball.x > width + ball.radius) {
+    ball.x -= width + ball.radius*2;
+  }
+  else if (ball.x < -ball.radius) {
+    ball.x += width + ball.radius*2;
+  };
+
+  if (ball.y > height + ball.radius) {
+    ball.y -= height + ball.radius*2;
+  }
+  else if (ball.y < -ball.radius) {
+    ball.y += height + ball.radius*2;
+  };
+}
+
+function drawBall(ball) {
+  circle(ball.x, ball.y, ball.radius*2);
 }
