fc9a3c947d59afebd17b9715a0e3824048b9cc88
PossiblyAxolotl <me@possiblyaxolotl.com>
2025-03-12 14:13:57 -0600

    Start blade spawning, clamp player in box, weird code to account for center origin, add player colour

--- projects/arrays-object-notation/sketch.js
+++ projects/arrays-object-notation/sketch.js
@@ -5,8 +5,9 @@
 // Extra for Experts:
 // - p5party online multiplayer
 
-const roomWidth = 500;
-const roomHeight = 500;
+const ROOM_WIDTH = 500;
+const ROOM_HEIGHT = 500;
+const ROOM_PADDING = 20;
 
 let shared;
 let player;
@@ -19,7 +20,7 @@ function preload() {
   );
 
   shared = partyLoadShared("global");
-  player = partyLoadMyShared({ pos: {x: 200, y: 20}, alive: true });
+  player = partyLoadMyShared({ pos: {x: 0, y: 0}, alive: true, colour: [random(255),random(255),random(255)] });
   players = partyLoadGuestShareds();
 }
 
@@ -32,19 +33,87 @@ function setup() {
   strokeJoin(ROUND);
 
   if (partyIsHost()) {
-    partySetShared(shared, { discs: [], inMatch: false });
+    partySetShared(shared, { discs: [], inMatch: false, discCountdown: 3, discCountdownReset: 3 });
   }
 }
 
 function draw() {
   background(0);
-  rect(width/2 - roomWidth/2, height/2 - roomHeight/2, roomWidth, roomHeight);
 
+  // processing
+  processPlayer();
+
+  // party host only -- make sure it doesn't double up on blade spawning
+  if (partyIsHost()) {
+    countdownToSaw();
+  }
+
+  // drawing
+  drawPlayers();
+  drawSaws();
+  drawWalls();
+}
+
+// Basic drawing
+function drawWalls() {
+  stroke("white");
+  rect(width/2 - ROOM_WIDTH/2 - ROOM_PADDING/2, height/2 - ROOM_HEIGHT/2 - ROOM_PADDING/2, ROOM_WIDTH + ROOM_PADDING, ROOM_HEIGHT + ROOM_PADDING);
+}
+
+function drawPlayers() {
+  // Player will be an image so anchor will be in the center
+  for (let p of players) {
+    stroke(p.colour);
+    rect(width/2 + p.pos.x - 5, height/2 + p.pos.y - 5, 10, 10);
+  }
+}
+
+// Player movement
+function processPlayer() {
   player.pos.x += horizontalButtons();
   player.pos.y += verticalButtons();
 
-  for (let p of players) {
-    rect(width/2 + p.pos.x, height/2 + p.pos.y, 10, 10);
+  clampPlayerPosition();
+}
+
+function clampPlayerPosition() {
+  // Horizontal clamp
+  if (Math.abs(player.pos.x) > ROOM_WIDTH/2) {
+    player.pos.x = ROOM_WIDTH/2 * Math.sign(player.pos.x);
+  }
+
+  // Vertical clamp
+  if (Math.abs(player.pos.y) > ROOM_HEIGHT/2) {
+    player.pos.y = ROOM_HEIGHT/2 * Math.sign(player.pos.y);
+  }
+}
+
+// Sawblades
+function createSaw() {
+  let newSaw = {
+    x: random(width/2-ROOM_WIDTH/2, width/2+ROOM_WIDTH/2),
+    y: random(height/2-ROOM_HEIGHT/2, height/2+ROOM_HEIGHT/2),
+    r: random(359),
+  };
+
+  return newSaw;
+}
+
+function drawSaws() {
+  for (let saw of shared.discs) {
+    circle(saw.x, saw.y, 50);
+  }
+}
+
+/// Host only
+function countdownToSaw() {
+  shared.discCountdown -= 0.1;
+
+  if (shared.discCountdown <= 0) {
+    shared.discCountdownReset -= 0.1;
+    shared.discCountdown = shared.discCountdownReset;
+
+    shared.discs.push(createSaw());
   }
 }
 
