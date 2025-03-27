54971f0aec5bda335a00b75162c2a6ef0c85bec5
PossiblyAxolotl <me@possiblyaxolotl.com>
2025-03-27 12:15:22 -0600

    add room size consts

--- projects/grid-based-game/sketch.js
+++ projects/grid-based-game/sketch.js
@@ -1,10 +1,13 @@
 // Project Title
 // Your Name
-// Date
+// Tueesday, April 8
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// amount of tiles wide and tall
+const ROOM_WIDTH = 16;
+const ROOM_HEIGHT = 16;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
